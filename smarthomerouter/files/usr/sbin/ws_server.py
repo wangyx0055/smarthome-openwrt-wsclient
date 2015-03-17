a2bf2864ff0a0c2a97d7a1b373fbd1032cc79197
root <xiangstudio@qq.com>
2015-03-17 18:43:55 +0800

    add notification

--- smarthomerouter/files/usr/sbin/ws_server.py
+++ smarthomerouter/files/usr/sbin/ws_server.py
@@ -8,6 +8,8 @@ import StringIO
 import sys
 import json
 import logging
+import time
+import threading
 from apitools import api_call 
 
 
@@ -23,7 +25,6 @@ if __name__ == "__main__":
   
     logger.debug("ws create connection")
     ws = websocket.create_connection("ws://182.92.232.157:8080")
-    #ws = websocket.create_connection("ws://192.168.56.103:1234")
     #ws = websocket.create_connection("wss://ws-dev.ezlink-wifi.com:8888")
     #ws = websocket.create_connection("ws://60.206.36.142:8888/")
     #ws = websocket.create_connection("ws://60.206.137.246:3000/")
@@ -45,7 +46,7 @@ if __name__ == "__main__":
         exit()
     logger.debug("token is %s" % token)
 
-    ws_access_obj_req = {"type":"connect","token":token,"sn":sn}
+    ws_access_obj_req = {"type":"connect","token":token,"sn":sn,"c_type":"router"}
     ws.send(json.dumps(ws_access_obj_req))
 
     received = ws.recv()
@@ -57,11 +58,38 @@ if __name__ == "__main__":
         logger.debug("not connected")
         exit()
     else:
-	logger.debug("connect response : '%s'" % message)
+	logger.debug("lizm connect response : '%s'" % message)
 	
+    def send_notification():
+    	wsid = 1
+    	while True:
+    	    time.sleep(5)
+    	    
+    	    logger.debug("send_notification begin execute ")
+    	    logger.debug("wsid = %d",wsid)
+    	    
+            api_json_rsp = api_call("net","get_network_associate_list","")
+            api_obj_rsp = json.loads(api_json_rsp)
+            ws_obj_rsp = {"type":"api_notification"}
+            ws_obj_rsp["data"] = api_obj_rsp['result']
+            ws_obj_rsp["wsid"] = '%d'%wsid
+            #ws_obj_rsp["wsid"] = "1234567890"
+            #ws_obj_rsp["from"] = ws_obj_req['from']
+            ws_obj_rsp["from"] = sn 
+            ws_json_rsp = json.dumps(ws_obj_rsp)
+            logger.debug("ws notification send: %s" % ws_json_rsp)
+            ws.send(ws_json_rsp)
+            wsid+=1
+    	    logger.debug("wsid = +1  %d",wsid)
+            
+    	    logger.debug("send_notification end execute, and next loop after 5s ")
+    	
+    thread = threading.Thread(target=send_notification)
+    thread.daemon = True
+    thread.start()
 
     while True:
-        logger.debug("waiting for websocket ...")
+        logger.debug("lizm waiting for websocket ...")
         try:
             received = ws.recv()
         except:
