35656d3cf7a3b6503edc32213338b5ce21bd9c26
root <xiangstudio@qq.com>
2015-03-19 17:49:51 +0800

    notification

--- smarthomerouter/files/usr/sbin/ws_server.py
+++ smarthomerouter/files/usr/sbin/ws_server.py
@@ -12,6 +12,8 @@ import time
 import threading
 from apitools import api_call 
 
+g_user_list = []
+tmp_user_list = []
 
 if __name__ == "__main__":
     websocket.enableTrace(True)
@@ -60,31 +62,79 @@ if __name__ == "__main__":
     else:
 	logger.debug("lizm connect response : '%s'" % message)
 	
-    def send_notification():
+    def send_notification(sn,islogin,msg):
+    	ws_obj_rsp = {"type":"api_notification"}
+    	ws_obj_rsp["wsid"] = 888
+    	ws_obj_rsp["from"] = sn 
+    	
+    	data = {"msgtype":"statistic"}
+    	
+    	if islogin == 1:
+    		nmsg = 'LogIn: '
+    	elif islogin == 0:
+    		nmsg = 'LogOut: '
+    		
+    	nmsg += msg["devicetype"]
+    	nmsg += ','
+    	#nmsg = nmsg.join(msg["devicename"])
+    	nmsg += msg["devicename"]
+    	nmsg += ','
+    	nmsg += msg["ipaddr"]
+    	#nmsg = nmsg.join(msg["ipaddr"])
+    	nmsg += ','
+    	nmsg += msg["macaddr"]
+    	
+    	data["message"] = nmsg
+    	
+    	ws_obj_rsp["data"] = data
+    	ws_json_rsp = json.dumps(ws_obj_rsp)
+    	ws.send(ws_json_rsp)
+    	
+    	logger.debug("send notification = %s",ws_json_rsp)
+    	
+	
+    def send_notification_thread():
     	wsid = 1
     	while True:
-    	    time.sleep(5)
+    	    time.sleep(3)
     	    
     	    logger.debug("send_notification begin execute ")
-    	    logger.debug("wsid = %d",wsid)
     	    
             api_json_rsp = api_call("net","get_network_associate_list","")
             api_obj_rsp = json.loads(api_json_rsp)
-            ws_obj_rsp = {"type":"api_notification"}
-            ws_obj_rsp["data"] = api_obj_rsp['result']
-            ws_obj_rsp["wsid"] = '%d'%wsid
-            #ws_obj_rsp["wsid"] = "1234567890"
-            #ws_obj_rsp["from"] = ws_obj_req['from']
-            ws_obj_rsp["from"] = sn 
-            ws_json_rsp = json.dumps(ws_obj_rsp)
-            logger.debug("ws notification send: %s" % ws_json_rsp)
-            ws.send(ws_json_rsp)
-            wsid+=1
-    	    logger.debug("wsid = +1  %d",wsid)
             
+            results = api_obj_rsp["result"]
+            logger.debug("lizm: len(results) %d" , len(results))
+            
+            for result in results:
+            	logger.debug("result: mac = %s",result["macaddr"])
+            	found = 0
+            	for i, x in enumerate(g_user_list):
+            		if x["macaddr"] == result["macaddr"]:
+            			found = 1
+            			break
+            	if found == 0:
+            		g_user_list.append(result)		
+            		logger.debug("we should notification mac  %s logger on",result["macaddr"])
+            		send_notification(sn,1,result)
+ 			
+            for j,y in enumerate(g_user_list):
+            	found = 0 
+            	for result in results:
+            		if y["macaddr"] == result["macaddr"]:
+            			found = 1
+            			break
+            	if found == 0:
+            		logger.debug("we should notification mac  %s logger off",y["macaddr"])
+            		g_user_list.remove(y)
+            		send_notification(sn,0,y)
+            		
+#            for j,y in enumerate(g_user_list):
+#            	logger.debug("now have user %s ",y["macaddr"])
+            	
     	    logger.debug("send_notification end execute, and next loop after 5s ")
     	
-    thread = threading.Thread(target=send_notification)
+    thread = threading.Thread(target=send_notification_thread)
     thread.daemon = True
     thread.start()
 
