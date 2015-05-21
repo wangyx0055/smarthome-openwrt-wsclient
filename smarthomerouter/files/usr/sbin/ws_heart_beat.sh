a7e5e995bd855c9234e4b567b63b5df07d3146b0
郝广宇 <haoguangyu@hiveview.com>
2015-05-21 16:57:49 +0800

    modify heart_beat

--- smarthomerouter/files/usr/sbin/ws_heart_beat.sh
+++ smarthomerouter/files/usr/sbin/ws_heart_beat.sh
@@ -2,6 +2,7 @@
 
 #Param process
 relogin=$1
+proto=`/sbin/uci get network.wan.proto`
 
 #Log File                                       
 logfile=/tmp/ws_client_launch.log
@@ -18,12 +19,15 @@ while true;
 do
 	# Delete the logfile if the size of logfile exceed 512K
 	logsize_limits
+
+	# Get current wan protocol
+        tmpproto=`/sbin/uci get network.wan.proto`
 	
 	process=`ps | grep ws_server.py`
 	result=`echo ${process} | grep -e "python /usr/sbin/ws_server.py*"`
 	
-	if [ -z "${result}" ];then
-		echo "`date`: websocket client is abnormal ">> ${logfile}
+	if [ -z "${result}" ] || [ ${proto} != ${tmpproto} ];then
+		echo "`date`: websocket client is abnormal, current proto is ${tmpproto}, last proto is ${proto} ">> ${logfile}
 		/usr/sbin/ws_kill.sh
 		/usr/sbin/ws_start.sh ${relogin} &
 	else
